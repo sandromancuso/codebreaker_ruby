c87697f9a9ee076f88631162a1129a4ef0c08618
Sandro Mancuso <sandromancuso@yahoo.com>
2011-09-03 23:06:42 +0100

    Initial commit

---
+++ spec/codebreaker/game_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+module Codebreaker
+
+  describe Game do
+    describe "#start" do
+      it "sends a welcome message" do
+        output = double('output')
+        game = Game.new(output)
+
+        output.should_receive(:puts).with('Welcome to Codebreaker!')
+
+        game.start
+      end
+
+      it "prompts the first guess" do
+        output = double('output')
+        game = Game.new(output)
+
+        output.should_receive(:puts).with('Enter guess:')
+      end
+    end
+  end
+end
